2d8260ba1e1e3b6c23e2551222f3d0f3cd55ca4f
Gustavo Paes <gustavo.paes@gmail.com>
2013-08-22 17:48:36 -0300

    Troca TAB por espaços

--- server.js
+++ server.js
@@ -10,115 +10,115 @@
  *
  */
 (function( PORTA ) {
-	"use strict";
+  "use strict";
 
-	var WebSocketServer = require('websocket').server,
-		http = require('http'),
-		fs = require('fs'),
-		path = require('path');
+  var WebSocketServer = require('websocket').server,
+    http = require('http'),
+    fs = require('fs'),
+    path = require('path');
 
-	/**
-	 * Clona um array.
-	 * @param {Array} a Array a ser clonado
-	 * @returns {Array}
-	 */
-	function cloneArray(a) {
-		var t = [], i = a.length;
-		while(i--)
-			t.push(a[i]);
+  /**
+   * Clona um array.
+   * @param {Array} a Array a ser clonado
+   * @returns {Array}
+   */
+  function cloneArray(a) {
+    var t = [], i = a.length;
+    while(i--)
+      t.push(a[i]);
 
-		return t.reverse();
-	}
+    return t.reverse();
+  }
 
-	/**
-	 * Remove do array de conexões as conexões
-	 * que não estão mais ativas.
-	 */
-	function garbageConnections() {
-		var i = connections.length, t = [];
-		while(i--) {
-			if(connections[i].connected === false) {
-				console.log('[garbage]\tRemovendo conexão inativa');
-				continue;
-			}
+  /**
+   * Remove do array de conexões as conexões
+   * que não estão mais ativas.
+   */
+  function garbageConnections() {
+    var i = connections.length, t = [];
+    while(i--) {
+      if(connections[i].connected === false) {
+        console.log('[garbage]\tRemovendo conexão inativa');
+        continue;
+      }
 
-			t.push(connections[i]);
-		}
+      t.push(connections[i]);
+    }
 
-		connections = t;
-	}
+    connections = t;
+  }
 
-	// Inicia o server HTTP.
-	// O websocket usará a mesma porta para a conexão,
-	// só que através do protocolo 'ws' e não 'http'.
-	var server = http.createServer(function(request, response) {
-		fs.readFile( path.resolve(__dirname, './index.html'), function(err, content) {
-			if( !!err === true ) {
-				response.writeHead(500, { 'Content-Type': 'text/html', 'charset': 'utf-8' });
-				response.end('Erro ao servir arquivo "index.html".<hr>' + JSON.stringify(err));
-			}
+  // Inicia o server HTTP.
+  // O websocket usará a mesma porta para a conexão,
+  // só que através do protocolo 'ws' e não 'http'.
+  var server = http.createServer(function(request, response) {
+    fs.readFile( path.resolve(__dirname, './index.html'), function(err, content) {
+      if( !!err === true ) {
+        response.writeHead(500, { 'Content-Type': 'text/html', 'charset': 'utf-8' });
+        response.end('Erro ao servir arquivo "index.html".<hr>' + JSON.stringify(err));
+      }
 
-			response.writeHead(200, { 'Content-Type': 'text/html', 'charset': 'utf-8' });
-			response.end(content);
-		} );
-	}).listen( PORTA );
+      response.writeHead(200, { 'Content-Type': 'text/html', 'charset': 'utf-8' });
+      response.end(content);
+    } );
+  }).listen( PORTA );
 
-	// Inicia o serviço Websocket em cima do HTTP
-	var wsServer = new WebSocketServer({
-		httpServer: server,
-		autoAcceptConnections: false
-	});
+  // Inicia o serviço Websocket em cima do HTTP
+  var wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+  });
 
-	// registra as conexões abertas
-	var connections = [];
+  // registra as conexões abertas
+  var connections = [];
 
-	// Armazena a última mensagem que o servidor recebeu.
-	// Essa mensagem será enviada para as novas conexões.
-	var lastMessage = null;
+  // Armazena a última mensagem que o servidor recebeu.
+  // Essa mensagem será enviada para as novas conexões.
+  var lastMessage = null;
 
-	// Dispara evento para quando receber uma conexão
-	wsServer.on('request', function(request) {
-		var connection = request.accept('echo-protocol', request.origin);
+  // Dispara evento para quando receber uma conexão
+  wsServer.on('request', function(request) {
+    var connection = request.accept('echo-protocol', request.origin);
 
-		console.log('Nova conexão.');
+    console.log('Nova conexão.');
 
-		// Envia a última mensagem recebida pelo servidor
-		if( !!lastMessage === true ) {
-			connection.sendUTF(lastMessage.utf8Data);
-		}
+    // Envia a última mensagem recebida pelo servidor
+    if( !!lastMessage === true ) {
+      connection.sendUTF(lastMessage.utf8Data);
+    }
 
-		// Evento para receber uma mensagem do cliente em questão
-		connection.on('message', function(message) {
-			// guarda a mensagem para as próximas conexões
-			lastMessage = message;
+    // Evento para receber uma mensagem do cliente em questão
+    connection.on('message', function(message) {
+      // guarda a mensagem para as próximas conexões
+      lastMessage = message;
 
-			// clona array de conexões
-			var conn, conns = cloneArray(connections);
+      // clona array de conexões
+      var conn, conns = cloneArray(connections);
 
-			// envia a mensagem recebida para todos que estão
-			// conectados ao websocket
-			while( (conn = conns.shift()) ) {
-				if (message.type === 'utf8') {
-					console.log('\n------------------\nMensagem recebida:\n------------------\n');
-					console.log(message.utf8Data);
+      // envia a mensagem recebida para todos que estão
+      // conectados ao websocket
+      while( (conn = conns.shift()) ) {
+        if (message.type === 'utf8') {
+          console.log('\n------------------\nMensagem recebida:\n------------------\n');
+          console.log(message.utf8Data);
 
-					!!conn === true &&
-						conn.connected === true &&
-							conn.sendUTF(message.utf8Data);
-				}
-			}
-	});
+          !!conn === true &&
+            conn.connected === true &&
+              conn.sendUTF(message.utf8Data);
+        }
+      }
+  });
 
-	// Evento disparado quando a conexão é fechada
-	connection.on('close', function(reasonCode, description) {
-		// força rodar o gargabeConnections
-		garbageConnections();
-	});
+  // Evento disparado quando a conexão é fechada
+  connection.on('close', function(reasonCode, description) {
+    // força rodar o gargabeConnections
+    garbageConnections();
+  });
 
-	// adiciona a nova conexão à lista de conexões
-	connections.push(connection);
-	});
+  // adiciona a nova conexão à lista de conexões
+  connections.push(connection);
+  });
 
-	// registra collection gargabe para as conexões inativas
-	setInterval(garbageConnections, 15000);
+  // registra collection gargabe para as conexões inativas
+  setInterval(garbageConnections, 15000);
 }(8080));
